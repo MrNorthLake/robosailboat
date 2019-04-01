468b464b23d9a5d627359f36cb72c8488a0888c0
connybackas <backasconny@gmail.com>
2019-04-01 15:52:37 +0300

    Implemented constructor and added two empty functions

--- src/main/java/fi/robosailboat/webservice/calculations/VelocityCalculator.java
+++ src/main/java/fi/robosailboat/webservice/calculations/VelocityCalculator.java
@@ -1,7 +1,24 @@
 package fi.robosailboat.webservice.calculations;
 
+import fi.robosailboat.webservice.boatCommunication.GpsPoint;
+
 public class VelocityCalculator {
 
-    public VelocityCalculator() {
+    private GpsPoint first;
+    private GpsPoint second;
+
+    public VelocityCalculator(GpsPoint first, GpsPoint second) {
+        first = first;
+        second = second;
+    }
+
+    public String calculateKmh(){
+        //Calculate velocity and return kmh
+        return "";
+    }
+
+    public String calcualteKnots(){
+        //Calulate velocity and return knots
+        return "";
     }
 }
