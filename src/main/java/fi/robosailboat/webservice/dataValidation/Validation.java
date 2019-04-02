3bc052fe848a26bcdf4f6d774e5a692d38b12524
connybackas <backasconny@gmail.com>
2019-04-02 10:35:56 +0300

    Created dataValidation package with Validation class

---
+++ src/main/java/fi/robosailboat/webservice/dataValidation/Validation.java
@@ -0,0 +1,8 @@
+package fi.robosailboat.webservice.dataValidation;
+
+import fi.robosailboat.webservice.boatCommunication.SensorData;
+
+public class Validation {
+
+    private SensorData latestData;
+}
