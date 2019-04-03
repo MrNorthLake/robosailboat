e96cf88d846c0293a9f88b13534e2392a945823a
connybackas <backasconny@gmail.com>
2019-04-03 13:18:27 +0300

    Implemented validate function that compares 2 sensorData objects and calculates percentage differnece

--- src/main/java/fi/robosailboat/webservice/dataValidation/Validator.java
+++ src/main/java/fi/robosailboat/webservice/dataValidation/Validator.java
@@ -1,6 +1,9 @@
 package fi.robosailboat.webservice.dataValidation;
 
 import fi.robosailboat.webservice.boatCommunication.SensorData;
+
+import java.math.BigDecimal;
+
 /*
     Validates the difference between latest data and expected data.
     If the difference is bigger than the max difference entered
@@ -11,12 +14,31 @@ public class Validator {
 
     private SensorData latestData;
     private SensorData expectedData;
-    int maxDiff;
+    int maxDiff; // Max differenece in percentage
 
 
     public Validator(SensorData latestData, SensorData expectedData, int maxDiff) {
         this.latestData = latestData;
         this.expectedData = expectedData;
         this.maxDiff = maxDiff;
+    }
+
+    public void validate(){
+
+        BigDecimal dirDiff = latestData.getDirection().subtract(expectedData.getDirection()).abs();
+        BigDecimal latDiff = latestData.getLatitud().subtract(expectedData.getLatitud()).abs();
+        BigDecimal lonDiff = latestData.getLongitud().subtract(expectedData.getLongitud()).abs();
+        BigDecimal comDiff = latestData.getCompassHeading().subtract(expectedData.getCompassHeading()).abs();
+
+        BigDecimal differenceSum = dirDiff.add(latDiff).add(lonDiff).add(comDiff);
+        BigDecimal expectedSum = expectedData.getDirection().add(expectedData.getLatitud())
+                .add(expectedData.getLongitud()).add(expectedData.getCompassHeading());
+
+        BigDecimal diffPercentage = differenceSum.divide(expectedSum).multiply(new BigDecimal(100));
+
+        if(diffPercentage.intValue() > maxDiff){
+            System.out.println("To big off a difference. Send route modification to boat!");
+        }
+
     }
 }
