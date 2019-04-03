2abc9db7c82937729ebf86f9e19c830cab854480
MrNorthLake <mikael.norrsjo@gmail.com>
2019-04-03 13:40:04 +0300

    mongo shit working

--- src/main/java/fi/robosailboat/webservice/robosailboatLib/repository/LoggingRepository.java
+++ src/main/java/fi/robosailboat/webservice/robosailboatLib/repository/LoggingRepository.java
@@ -2,8 +2,6 @@ package fi.robosailboat.webservice.robosailboatLib.repository;
 
 import fi.robosailboat.webservice.boatCommunication.dto.SensorData;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface LoggingRepository extends MongoRepository<SensorData,String> {
 }
