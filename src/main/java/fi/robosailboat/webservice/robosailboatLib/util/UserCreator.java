5dd93a1cd273ad3ebf251e8246d96b50e34a41d9
MrNorthLake <mikael.norrsjo@gmail.com>
2019-04-08 11:38:03 +0300

    untracked the usercreator file

--- src/main/java/fi/robosailboat/webservice/robosailboatLib/util/UserCreator.java
+++ src/main/java/fi/robosailboat/webservice/robosailboatLib/util/UserCreator.java
@@ -9,24 +9,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class UserCreator {
 
-	private static String USERNAME = "test2";
-	private static String PASSWORD = "test2";
-	private static BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder();
-	@Autowired
-	static
-	UserRepository userRepository;
+    private static String USERNAME = "test2";
+    private static String PASSWORD = "test2";
+    private static BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder();
+    @Autowired
+    static
+    UserRepository userRepository;
 
 
 
 
 
-	public static void createUser(){
-		User user = new User();
-		user.setUserName(USERNAME);
-		user.setPassword(encoder.encode(PASSWORD));
-		System.out.println("New User Initialized | UserName: " + user.getUserName() + " | password: "+ user.getPassword());
+    public static void createUser(){
+        User user = new User();
+        user.setUserName(USERNAME);
+        user.setPassword(encoder.encode(PASSWORD));
+        System.out.println("New User Initialized | UserName: " + user.getUserName() + " | password: "+ user.getPassword());
 
-		userRepository.insert(user);
-	}
+        userRepository.insert(user);
+    }
 
 }
