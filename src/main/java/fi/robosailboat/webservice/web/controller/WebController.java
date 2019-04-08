7c0598c457159e8c4d4ecff35b16565f3b704a51
connybackas <backasconny@gmail.com>
2019-04-08 14:25:23 +0300

    Implemented UI attributes

--- src/main/java/fi/robosailboat/webservice/web/controller/WebController.java
+++ src/main/java/fi/robosailboat/webservice/web/controller/WebController.java
@@ -1,5 +1,8 @@
 package fi.robosailboat.webservice.web.controller;
 
+import fi.robosailboat.webservice.boatCommunication.dto.SensorData;
+import fi.robosailboat.webservice.robosailboatLib.repository.LoggingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
+    @Autowired
+    LoggingRepository db;
+
     @RequestMapping("/")
     public String home(Model model) {
+        SensorData latestData = db.findTopByOrderByCreatedDesc();
         model.addAttribute("wind", "wind test");
+        model.addAttribute("heading", latestData.getCompassHeading());
+        model.addAttribute("position", latestData.getLatitude()+", "+latestData.getLongitude());
         return "index";
     }
 
