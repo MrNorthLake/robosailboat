38ad090db86ef96bf17c482d07c9af93229c15e2
MrNorthLake <mikael.norrsjo@gmail.com>
2019-04-08 11:57:22 +0300

    changed the password hash method

--- src/main/java/fi/robosailboat/webservice/WebserviceApplication.java
+++ src/main/java/fi/robosailboat/webservice/WebserviceApplication.java
@@ -1,6 +1,7 @@
 package fi.robosailboat.webservice;
 
-import fi.robosailboat.webservice.robosailboatLib.util.UserCreator;
+import fi.robosailboat.webservice.robosailboatLib.util.PasswordCreator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -10,7 +11,7 @@ public class WebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
-		//UserCreator.createUser();
+		//PasswordCreator.createPassword();
 	}
 
 }
