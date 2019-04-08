8008fde2a9b8f8d47dd39104727af2a4348a786a
MrNorthLake <mikael.norrsjo@gmail.com>
2019-04-08 15:03:32 +0300

    added csv writer

--- src/main/java/fi/robosailboat/webservice/web/controller/WebController.java
+++ src/main/java/fi/robosailboat/webservice/web/controller/WebController.java
@@ -2,16 +2,22 @@ package fi.robosailboat.webservice.web.controller;
 
 import fi.robosailboat.webservice.boatCommunication.dto.SensorData;
 import fi.robosailboat.webservice.robosailboatLib.repository.LoggingRepository;
+import fi.robosailboat.webservice.web.service.WriteDataToCSV;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class WebController {
 
     @Autowired
-    LoggingRepository db;
+    LoggingRepository loggingRepository;
 
     @RequestMapping("/")
     public String home(Model model) {
@@ -25,5 +31,16 @@ public class WebController {
     @RequestMapping("/log")
     public String log() {
         return "log";
+    }
+
+
+    @GetMapping("/log/download/Log.csv")
+    public void downloadCSV(HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; file=customers.csv");
+
+        List<SensorData> logs = loggingRepository.findAll();
+
+         WriteDataToCSV.writeDataToCsvUsingStringArray(response.getWriter(), logs);
     }
 }
