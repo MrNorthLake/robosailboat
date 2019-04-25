7568e12d9543b37136fa8b7acdad3b1765ed4d69
MrNorthLake <mikael.norrsjo@gmail.com>
2019-04-25 14:06:17 +0300

    refactored the mqtt service

--- src/main/java/fi/robosailboat/webservice/boatCommunication/controller/VesselDataTransferController.java
+++ src/main/java/fi/robosailboat/webservice/boatCommunication/controller/VesselDataTransferController.java
@@ -3,6 +3,7 @@ package fi.robosailboat.webservice.boatCommunication.controller;
 import fi.robosailboat.webservice.boatCommunication.dto.Command;
 import fi.robosailboat.webservice.boatCommunication.dto.SensorData;
 import fi.robosailboat.webservice.robosailboatLib.repository.LoggingRepository;
+import fi.robosailboat.webservice.weatherStationCommunication.SimpleMqttCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
