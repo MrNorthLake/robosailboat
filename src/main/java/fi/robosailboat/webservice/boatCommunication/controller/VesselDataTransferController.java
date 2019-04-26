3b77a526b336f0389c58a1112422deff6ecc704b
Maiken <maiken.soderlund@gmail.com>
2019-04-26 13:59:09 +0300

    Changes in Command.

--- src/main/java/fi/robosailboat/webservice/boatCommunication/controller/VesselDataTransferController.java
+++ src/main/java/fi/robosailboat/webservice/boatCommunication/controller/VesselDataTransferController.java
@@ -19,7 +19,7 @@ public class VesselDataTransferController {
 
     @PostMapping(value = "/sensorData", consumes = "application/json", produces = "application/json")
     @ResponseBody
-    public String postData(@RequestBody SensorData values) {
+    public Command postData(@RequestBody SensorData values) {
 
 
        /* this is just for testing*/
