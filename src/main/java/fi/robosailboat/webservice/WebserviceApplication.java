5b7a1d4e89cc0030bd88327574eeb116f0a00011
MrNorthLake <mikael.norrsjo@gmail.com>
2019-04-29 12:48:50 +0300

    bajs

--- src/main/java/fi/robosailboat/webservice/WebserviceApplication.java
+++ src/main/java/fi/robosailboat/webservice/WebserviceApplication.java
@@ -2,19 +2,22 @@ package fi.robosailboat.webservice;
 
 //import fi.robosailboat.webservice.robosailboatLib.util.PasswordCreator;
 
-import fi.robosailboat.webservice.weatherStationCommunication.SimpleMqttCallback;
+
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@EnableAutoConfiguration
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WebserviceApplication {
 
 	public static void main(String[] args) throws MqttException {
 		SpringApplication.run(WebserviceApplication.class, args);
 		//PasswordCreator.createPassword();
-		SimpleMqttCallback mqtt = new SimpleMqttCallback();
+
+
 	}
 
 }
