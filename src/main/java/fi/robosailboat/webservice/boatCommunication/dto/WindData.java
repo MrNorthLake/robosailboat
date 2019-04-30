e3a5e50b89f05ba5d7c72b58e3a8bd2965850ef3
Maiken <maiken.soderlund@gmail.com>
2019-04-30 08:57:22 +0300

    Added class WindData.

---
+++ src/main/java/fi/robosailboat/webservice/boatCommunication/dto/WindData.java
@@ -0,0 +1,11 @@
+package fi.robosailboat.webservice.boatCommunication.dto;
+
+import lombok.Data;
+
+
+@Data
+public class WindData {
+
+    private double windDirection;
+    private double windSpeed;
+}
