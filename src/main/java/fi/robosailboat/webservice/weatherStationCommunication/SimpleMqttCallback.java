f91c7d29cfb728a5681ad07571de1c21ea8532f6
MrNorthLake <mikael.norrsjo@gmail.com>
2019-05-02 10:00:52 +0300

    added front end part

--- src/main/java/fi/robosailboat/webservice/weatherStationCommunication/SimpleMqttCallback.java
+++ src/main/java/fi/robosailboat/webservice/weatherStationCommunication/SimpleMqttCallback.java
@@ -1,26 +1,41 @@
 package fi.robosailboat.webservice.weatherStationCommunication;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.json.JSONParser;
 import org.eclipse.paho.client.mqttv3.*;
+import org.json.JSONObject;
 
 @Slf4j
 public class SimpleMqttCallback implements MqttCallback {
 
-    private static final String CONNECTION_URL = "tcp://m24.cloudmqtt.com:14846";
-    private static final String SUBSCRIPTION = "wind_data";
+    private static final String CONNECTION_URI = "tcp://m24.cloudmqtt.com:14846";
+    private static final String SUBSCRIPTION = "weather";
+    private static final String USERNAME = "yirnfmrp";
+    private static final String PASSWORD = "yhxQJ-e_JUMg";
     private MqttClient client;
-    private static double windDirection;
+    private static WeatherDTO latestWeather;
 
     public SimpleMqttCallback() {
         try {
-            this.client = new MqttClient(CONNECTION_URL, MqttClient.generateClientId());
+            this.client = new MqttClient(CONNECTION_URI, "test");
             this.client.setCallback(this);
+            this.client.connect(initOptions());
             this.client.subscribe(SUBSCRIPTION);
-            this.client.connect();
         } catch (MqttException e) {
             log.error("Mqtt error: " + e);
         }
 
+    }
+
+    private MqttConnectOptions initOptions(){
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        return options;
     }
 
     @Override
@@ -30,7 +45,12 @@ public class SimpleMqttCallback implements MqttCallback {
 
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
-        log.info("Message received:\n\t" + new String(message.getPayload()));
+        String stringParse =  new String(message.getPayload());
+        log.info("Message received");
+
+        latestWeather = new ObjectMapper().readValue(stringParse, WeatherDTO.class);
+        log.info("object parsed. direction: " + latestWeather.getDirection() + " | speed: " + latestWeather.getSpeed()
+         + " | timestamp: " + latestWeather.getTimestamp());
     }
 
     @Override
@@ -38,7 +58,8 @@ public class SimpleMqttCallback implements MqttCallback {
         log.info("Mqtt Delivery Complete");
     }
 
-    public static double getLatestWeather() {
-        return windDirection;
+    public static WeatherDTO getLatestWeather() {
+        log.info("windDirection: " + latestWeather);
+        return latestWeather;
     }
 }
